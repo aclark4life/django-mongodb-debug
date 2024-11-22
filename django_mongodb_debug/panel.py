d841f009a2bd6dddaa0a1b51d32cfb48772495b0
Jeffrey A. Clark <aclark@aclark.net>
2024-11-21 19:42:52 -0500

    Add wrap_cursor

--- django_mongodb_debug/panel.py
+++ django_mongodb_debug/panel.py
@@ -1,3 +1,10 @@
+import contextlib
+import contextvars
+import datetime
+import django
+import json
+
+
 from collections import defaultdict
 from copy import copy
 
@@ -5,19 +12,26 @@ from asgiref.sync import sync_to_async
 from django.db import connections
 from django.urls import path
 from django.utils.translation import gettext_lazy as _, ngettext
+from django.utils.encoding import force_str
 
 from debug_toolbar import settings as dt_settings
 from debug_toolbar.forms import SignedDataForm
 from debug_toolbar.panels import Panel
 from debug_toolbar.panels.sql import views
 from debug_toolbar.panels.sql.forms import SQLSelectForm
-from debug_toolbar.panels.sql.tracking import wrap_cursor
 from debug_toolbar.panels.sql.utils import (
     contrasting_color_generator,
     is_select_query,
     reformat_sql,
 )
 from debug_toolbar.utils import render_stacktrace
+from debug_toolbar.utils import get_stack_trace, get_template_info
+from time import perf_counter
+
+# Prevents SQL queries from being sent to the DB. It's used
+# by the TemplatePanel to prevent the toolbar from issuing
+# additional queries.
+allow_sql = contextvars.ContextVar("debug-toolbar-allow-sql", default=True)
 
 
 def _similar_query_key(query):
@@ -45,6 +59,186 @@ def _process_query_groups(query_groups, databases, colors, name):
             counts[alias] += count
     for alias, db_info in databases.items():
         db_info[f"{name}_count"] = counts[alias]
+
+
+def wrap_cursor(connection):
+    # When running a SimpleTestCase, Django monkey patches some DatabaseWrapper
+    # methods, including .cursor() and .chunked_cursor(), to raise an exception
+    # if the test code tries to access the database, and then undoes the monkey
+    # patching when the test case is finished.  If we monkey patch those methods
+    # also, Django's process of undoing those monkey patches will fail.  To
+    # avoid this failure, and because database access is not allowed during a
+    # SimpleTestCase anyway, skip applying our instrumentation monkey patches if
+    # we detect that Django has already monkey patched DatabaseWrapper.cursor().
+    if isinstance(connection.cursor, django.test.testcases._DatabaseFailure):
+        return
+    if not hasattr(connection, "_djdt_cursor"):
+        connection._djdt_cursor = connection.cursor
+        connection._djdt_chunked_cursor = connection.chunked_cursor
+        connection._djdt_logger = None
+
+        def cursor(*args, **kwargs):
+            # Per the DB API cursor() does not accept any arguments. There's
+            # some code in the wild which does not follow that convention,
+            # so we pass on the arguments even though it's not clean.
+            # See:
+            # https://github.com/django-commons/django-debug-toolbar/pull/615
+            # https://github.com/django-commons/django-debug-toolbar/pull/896
+            logger = connection._djdt_logger
+            cursor = connection._djdt_cursor(*args, **kwargs)
+            if logger is None:
+                return cursor
+            mixin = NormalCursorMixin if allow_sql.get() else ExceptionCursorMixin
+            return patch_cursor_wrapper_with_mixin(cursor.__class__, mixin)(
+                cursor.cursor, connection, logger
+            )
+
+        def chunked_cursor(*args, **kwargs):
+            # prevent double wrapping
+            # solves https://github.com/django-commons/django-debug-toolbar/issues/1239
+            logger = connection._djdt_logger
+            cursor = connection._djdt_chunked_cursor(*args, **kwargs)
+            if logger is not None and not isinstance(cursor, DjDTCursorWrapperMixin):
+                mixin = NormalCursorMixin if allow_sql.get() else ExceptionCursorMixin
+                return patch_cursor_wrapper_with_mixin(cursor.__class__, mixin)(
+                    cursor.cursor, connection, logger
+                )
+            return cursor
+
+        connection.cursor = cursor
+        connection.chunked_cursor = chunked_cursor
+
+
+def patch_cursor_wrapper_with_mixin(base_wrapper, mixin):
+    class DjDTCursorWrapper(mixin, base_wrapper):
+        pass
+
+    return DjDTCursorWrapper
+
+
+class DjDTCursorWrapperMixin:
+    def __init__(self, cursor, db, logger):
+        super().__init__(cursor, db)
+        # logger must implement a ``record`` method
+        self.logger = logger
+
+
+class ExceptionCursorMixin(DjDTCursorWrapperMixin):
+    """
+    Wraps a cursor and raises an exception on any operation.
+    Used in Templates panel.
+    """
+
+    def __getattr__(self, attr):
+        raise SQLQueryTriggered()
+
+
+class NormalCursorMixin(DjDTCursorWrapperMixin):
+    """
+    Wraps a cursor and logs queries.
+    """
+
+    def _decode(self, param):
+        # If a sequence type, decode each element separately
+        if isinstance(param, (tuple, list)):
+            return [self._decode(element) for element in param]
+
+        # If a dictionary type, decode each value separately
+        if isinstance(param, dict):
+            return {key: self._decode(value) for key, value in param.items()}
+
+        # make sure datetime, date and time are converted to string by force_str
+        CONVERT_TYPES = (datetime.datetime, datetime.date, datetime.time)
+        try:
+            return force_str(param, strings_only=not isinstance(param, CONVERT_TYPES))
+        except UnicodeDecodeError:
+            return "(encoded string)"
+
+    def _last_executed_query(self, sql, params):
+        """Get the last executed query from the connection."""
+        # Django's psycopg3 backend creates a new cursor in its implementation of the
+        # .last_executed_query() method.  To avoid wrapping that cursor, temporarily set
+        # the DatabaseWrapper's ._djdt_logger attribute to None.  This will cause the
+        # monkey-patched .cursor() and .chunked_cursor() methods to skip the wrapping
+        # process during the .last_executed_query() call.
+        self.db._djdt_logger = None
+        try:
+            return self.db.ops.last_executed_query(self.cursor, sql, params)
+        finally:
+            self.db._djdt_logger = self.logger
+
+    def _record(self, method, sql, params):
+        alias = self.db.alias
+        vendor = self.db.vendor
+
+        if vendor == "postgresql":
+            # The underlying DB connection (as opposed to Django's wrapper)
+            conn = self.db.connection
+
+        start_time = perf_counter()
+        try:
+            return method(sql, params)
+        finally:
+            stop_time = perf_counter()
+            duration = (stop_time - start_time) * 1000
+            _params = ""
+            with contextlib.suppress(TypeError):
+                # object JSON serializable?
+                _params = json.dumps(self._decode(params))
+            template_info = get_template_info()
+
+            # Sql might be an object (such as psycopg Composed).
+            # For logging purposes, make sure it's str.
+            if vendor == "postgresql" and not isinstance(sql, str):
+                if isinstance(sql, bytes):
+                    sql = sql.decode("utf-8")
+                else:
+                    sql = sql.as_string(conn)
+            else:
+                sql = str(sql)
+
+            kwargs = {
+                "vendor": vendor,
+                "alias": alias,
+                "sql": self._last_executed_query(sql, params),
+                "duration": duration,
+                "raw_sql": sql,
+                "params": _params,
+                "raw_params": params,
+                "stacktrace": get_stack_trace(skip=2),
+                "template_info": template_info,
+            }
+
+            if vendor == "postgresql":
+                # If an erroneous query was ran on the connection, it might
+                # be in a state where checking isolation_level raises an
+                # exception.
+                try:
+                    iso_level = conn.isolation_level
+                except conn.InternalError:
+                    iso_level = "unknown"
+
+                kwargs.update(
+                    {
+                        "iso_level": iso_level,
+                    }
+                )
+
+            # We keep `sql` to maintain backwards compatibility
+            self.logger.record(**kwargs)
+
+    def callproc(self, procname, params=None):
+        return self._record(super().callproc, procname, params)
+
+    def execute(self, sql, params=None):
+        return self._record(super().execute, sql, params)
+
+    def executemany(self, sql, param_list):
+        return self._record(super().executemany, sql, param_list)
+
+
+class SQLQueryTriggered(Exception):
+    """Thrown when template panel triggers a query"""
 
 
 class MongoPanel(Panel):
